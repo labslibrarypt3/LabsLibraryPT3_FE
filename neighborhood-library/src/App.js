ff51a624153dfa9187c00c2577eef9b700f6062f
iradsanchez <ira.d.sanchez@gmail.com>
2019-05-15 19:37:58 -0700

    Add activeClassName And render Properties To App's NavLinks

--- neighborhood-library/src/App.js
+++ neighborhood-library/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import { NavLink } from "react-router-dom";
 
+//component imports
+import { Search, Account, AddBook, MyShelf } from "./components";
+
 function App() {
   return (
     <div className="App">
@@ -9,10 +12,34 @@ function App() {
         <h2>Neighborhood Library</h2>
       </header>
       <nav>
-        <NavLink to="/addBook">Add a Book</NavLink>
-        <NavLink to="/myShelf">My Shelf</NavLink>
-        <NavLink to="/search">Search</NavLink>
-        <NavLink to="/account">Account</NavLink>
+        <NavLink
+          to="/addBook"
+          activeClassName="selected"
+          render={props => <AddBook {...props} books={bookData} />}
+        >
+          Add a Book
+        </NavLink>
+        <NavLink
+          to="/myShelf"
+          activeClassName="selected"
+          render={props => <MyShelf {...props} books={bookData} />}
+        >
+          My Shelf
+        </NavLink>
+        <NavLink
+          to="/search"
+          activeClassName="selected"
+          render={props => <Search {...props} books={bookData} />}
+        >
+          Search
+        </NavLink>
+        <NavLink
+          to="/account"
+          activeClassName="selected"
+          render={props => <Account {...props} books={bookData} />}
+        >
+          Account
+        </NavLink>
       </nav>
     </div>
   );
