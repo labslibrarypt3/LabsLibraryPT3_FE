01b4c0691c1e32acb92543f129c66398706718e4
iradsanchez <ira.d.sanchez@gmail.com>
2019-05-21 07:35:52 -0700

    Implemented Axios
    Returns CORS error in console.log

--- neighborhood-library/src/App.js
+++ neighborhood-library/src/App.js
@@ -1,53 +1,48 @@
 import React from "react";
 import "./App.css";
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
+import axios from "axios";
 
 //component imports
-import Search from './components/Search/Search';
-import Account from './components/Account/Account';
-import AddBook from './components/AddBook/AddBook';
-import MyShelf from './components/MyShelf/MyShelf';
-import Nav from './components/Nav/Nav';
+import Search from "./components/Search/Search";
+import Account from "./components/Account/Account";
+import AddBook from "./components/AddBook/AddBook";
+import MyShelf from "./components/MyShelf/MyShelf";
+import Nav from "./components/Nav/Nav";
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state= {
+    this.state = {
       data: []
-    }
+    };
   }
 
-  async componentDidMount(){
-    
-    const response = await fetch( 'https://pt3-neighborhood-library-back.herokuapp.com/account');
-    const data = await response.json();
-    this.setState({data: data});
-    
-
+  componentDidMount() {
+    axios
+      .get("https://pt3-neighborhood-library-back.herokuapp.com/account")
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
-
-  render(){
-    
+  render() {
     return (
-    <div className="App">
+      <div className="App">
+        <header className="App-header">
+          <h2>Neighborhood Library</h2>
+        </header>
 
-      <header className="App-header">
-        <h2>Neighborhood Library</h2>
-        
-      </header>
+        <Nav />
 
-      <Nav/>
-
-      <div className="main">
-        <Route path="/add-book/:id" component={AddBook} />
-        <Route path="/account/:id/" component={Account} />
-        <Route path="/search" component={Search} />
-        <Route path="/my-shelf/:id" component={MyShelf} />
+        <div className="main">
+          <Route path="/add-book/:id" component={AddBook} />
+          <Route path="/account/:id/" component={Account} />
+          <Route path="/search" component={Search} />
+          <Route path="/my-shelf/:id" component={MyShelf} />
+        </div>
       </div>
-    </div>
-)  
-}
+    );
+  }
 }
 
 export default App;
