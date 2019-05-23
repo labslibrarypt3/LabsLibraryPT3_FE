183728aaac1bc3ae6773c6303a49c2d5ed4a478b
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-05-23 00:23:34 -0400

    added search input from

--- neighborhood-library/src/components/Search/Search.js
+++ neighborhood-library/src/components/Search/Search.js
@@ -1,13 +1,44 @@
 import React from "react";
 import Nav from "../Nav/Nav";
+import MapsContainer from "./MapsContainer";
 
-function Search() {
-  return (
-    <div>
-      <h2>this will be the search page</h2>
-      <Nav />
-    </div>
-  );
+class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchInput: ''
+    }
+  }
+
+  handleInput = e => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+  }
+  render() {
+    return (
+      <div>
+        <Nav />
+        <h2>this will be the search page</h2>
+        <form>
+          <input
+            type='text'
+            name='search'
+            placeholder='search'
+            value={this.state.searchInput}
+            onChange={this.state.handleInput}
+          />
+        </form>
+        <MapsContainer />
+      </div>
+    );
+  }
 }
 
 export default Search;
