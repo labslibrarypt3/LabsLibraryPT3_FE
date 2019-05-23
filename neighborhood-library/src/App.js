94dfa676bc5c5d6a4bfdabe733bc6f603dd8d6d3
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-05-22 22:47:50 -0400

    code clean up

--- neighborhood-library/src/App.js
+++ neighborhood-library/src/App.js
@@ -17,30 +17,33 @@ class App extends React.Component {
     }
   }
 
-  componentDidMount(){
-    const response = await fetch( /*URL GOES HERE*/);
-    const data = await response.json();
-    this.setState({data: data});
-
-  }
+  // Add call to pull user data
+  // componentDidMount(){
+  //   const response = await fetch( /*URL GOES HERE*/);
+  //   const data = await response.json();
+  //   this.setState({data: data});
+  //
+  // }
 
   render(){
-    <div className="App">
+    return (
+      <div className="App">
 
-      <header className="App-header">
-        <h2>Neighborhood Library</h2>
+        <header className="App-header">
+          <h2>Neighborhood Library</h2>
 
-      </header>
+        </header>
 
-      <Nav/>
+        <Nav/>
 
-      <div className="main">
-        <Route path="/add-book/:id" component={AddBook} />
-        <Route path="/account/:id/" component={Account} />
-        <Route path="/search" component={Search} />
-        <Route path="/my-shelf/:id" component={MyShelf} />
+        <div className="main">
+          <Route path="/add-book/:id" component={AddBook} />
+          <Route path="/account/:id/" component={Account} />
+          <Route path="/search" component={Search} />
+          <Route path="/my-shelf/:id" component={MyShelf} />
+        </div>
       </div>
-    </div>
+    )
   }
 }
 
