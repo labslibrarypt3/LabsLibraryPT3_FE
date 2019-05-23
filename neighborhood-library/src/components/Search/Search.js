9bdc1fa40c547958c6aa89afba2ac6275be9dd1f
kammagik <kammagik@yahoo.com>
2019-05-22 22:35:11 -0400

    once implemented search is functional

--- neighborhood-library/src/components/Search/Search.js
+++ neighborhood-library/src/components/Search/Search.js
@@ -1,13 +1,43 @@
-import React from "react";
-import Nav from "../Nav/Nav";
+// import React from "react";
+// import Nav from "../Nav/Nav";
 
-function Search() {
-  return (
-    <div>
-      <h2>this will be the search page</h2>
-      <Nav />
-    </div>
-  );
+// function Search() {
+//   return (
+//     <div>
+//       <h2>this will be the search page</h2>
+//       <Nav />
+//     </div>
+//   );
+// }
+
+// export default Search;
+import React, { Component } from 'react'
+
+class Search extends Component {
+state = {
+  query: '',
 }
 
-export default Search;
+handleInputChange = () => {
+  this.setState({
+    query: this.search.value
+  })
+}
+
+render() {
+  return (
+    
+    <form>
+      <input
+        placeholder="Search for..."
+        ref={input => this.search = input}
+        onChange={this.handleInputChange}
+      />
+      <p>{this.state.query}</p>
+    </form>
+  )
+}
+}
+
+export default Search
+
