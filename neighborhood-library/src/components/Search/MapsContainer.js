e5b115e9be4043f518ca6d962ea4cd6ab6d3168e
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-05-23 00:25:04 -0400

    created container for embedding map added iframe to display map

---
+++ neighborhood-library/src/components/Search/MapsContainer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class MapsContainer extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <iframe
+          width="600"
+          height="450"
+          frameborder="0"
+          style="border:0"
+          src="https://www.google.com/maps/embed/v1/undefined?origin=...&q=...&destination=...&center=...&zoom=...&key={process.env.REACT_APP_MAPS_KEY}" 
+          allowfullscreen>
+        </iframe>
+      </div>
+    )
+  }
+}
