1142d6a14e1dd7d82a9da57de3cac304df8751b4
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-05-25 13:25:28 -0400

    added export for component

--- neighborhood-library/src/components/Search/MapsContainer.js
+++ neighborhood-library/src/components/Search/MapsContainer.js
@@ -13,10 +13,12 @@ class MapsContainer extends Component {
           height="450"
           frameborder="0"
           style="border:0"
-          src="https://www.google.com/maps/embed/v1/undefined?origin=...&q=...&destination=...&center=...&zoom=...&key={process.env.REACT_APP_MAPS_KEY}" 
+          src="https://www.google.com/maps/embed/v1/undefined?origin=...&q=...&destination=...&center=...&zoom=...&key={process.env.REACT_APP_MAPS_KEY}"
           allowfullscreen>
         </iframe>
       </div>
     )
   }
 }
+
+export default MapsContainer;
