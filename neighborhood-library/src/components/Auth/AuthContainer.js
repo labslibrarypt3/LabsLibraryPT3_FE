94eb75f7b057a756018d8ec3518487bbb1287592
iradsanchez <ira.d.sanchez@gmail.com>
2019-06-08 11:49:31 -0700

    Add Flexbox to Hamburger

--- neighborhood-library/src/components/Auth/AuthContainer.js
+++ neighborhood-library/src/components/Auth/AuthContainer.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import AuthPostData from './AuthPostData';
+import AuthPostData from "./AuthPostData";
 // import Login from './Login';
 // import Register from './Register';
-import FacebookLogin from 'react-facebook-login';
-import GoogleLogin from 'react-google-login';
-import {Redirect} from 'react-router-dom';
+import FacebookLogin from "react-facebook-login";
+import GoogleLogin from "react-google-login";
+import { Redirect } from "react-router-dom";
 // import { Link } from "react-router-dom";
 
 class AuthContainer extends Component {
@@ -12,74 +12,69 @@ class AuthContainer extends Component {
     super(props);
     this.state = {
       loggedIn: false,
-      redirectToReferrer:false
+      redirectToReferrer: false
     };
     this.callback = this.callback.bind(this);
   }
-  callback (res, type){
+  callback(res, type) {
+    let userData;
 
-  let userData;
-
-  if( type ==='facebook'&& res.email){
-    userData = {
-      name: res.name,
-      provider:type,
-      email:res.email,
-      provider_id:res.id,
-      token:res.accessToken
+    if (type === "facebook" && res.email) {
+      userData = {
+        name: res.name,
+        provider: type,
+        email: res.email,
+        provider_id: res.id,
+        token: res.accessToken
+      };
     }
 
-  }
-
-  if( type ==='google'&& res.w3.U3){
+    if (type === "google" && res.w3.U3) {
       userData = {
         name: res.w3.ig,
-        provider:type,
-        email:res.w3.U3,
-        provider_id:res.El,
-        token:res.Zi.access_token
-      }
+        provider: type,
+        email: res.w3.U3,
+        provider_id: res.El,
+        token: res.Zi.access_token
+      };
+    }
+
+    AuthPostData(type, userData);
   }
 
-  AuthPostData(type,userData);
-}
-
   render() {
-   
-    if(this.state.redirectToReferrer){
-      return (<Redirect to ={'/account'}/>
-      )}
-    
-      const responseGoogle = (response) => {
+    if (this.state.redirectToReferrer) {
+      return <Redirect to={"/account"} />;
+    }
 
-        this.callback(response, 'google');
-      }
-      
-      const responseFacebook = (response) => {
+    const responseGoogle = response => {
+      this.callback(response, "google");
+    };
 
-        this.callback(response, 'facebook');
-      }
-      
-      const componentClicked =(response) => {
+    const responseFacebook = response => {
+      this.callback(response, "facebook");
+    };
 
-      }
-      return(
-        <div>
+    const componentClicked = response => {};
+    return (
+      <div>
         <FacebookLogin
-    appId="2561795117185670"
-    autoLoad={true}
-    fields="name,email,picture"
-    onClick={componentClicked}
-    callback={responseFacebook} />
+          appId="2561795117185670"
+          autoLoad={true}
+          fields="name,email,picture"
+          onClick={componentClicked}
+          callback={responseFacebook}
+          className="login-button"
+        />
 
-     <GoogleLogin
-    clientId="276277558253-h0h6fqj0m26r3a20998jeplb75r097g7.apps.googleusercontent.com"
-    buttonText="Login"
-    onSuccess={responseGoogle}
-    onFailure={responseGoogle}
-    cookiePolicy={'single_host_origin'}
-    
-  />
+        <GoogleLogin
+          clientId="276277558253-h0h6fqj0m26r3a20998jeplb75r097g7.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={"single_host_origin"}
+          className="login-button"
+        />
 
         {/* <Login />
         <Register />
@@ -91,8 +86,7 @@ class AuthContainer extends Component {
         )}
       </div> */}
       </div>
-      )
+    );
   }
-    
 }
 export default AuthContainer;
