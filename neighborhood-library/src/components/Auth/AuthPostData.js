2310a94b08f392ac637fe19fffeff8f58e88461e
unknown <tom@edgegluer.com>
2019-06-13 23:20:27 -0400

    Add localstorage for front end axios

--- neighborhood-library/src/components/Auth/AuthPostData.js
+++ neighborhood-library/src/components/Auth/AuthPostData.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default function AuthPostData(type,userData){
     
-    console.log(userData)
+    // console.log(userData)
 
     return new Promise((resolve, reject) =>{
         const endpoint = 'http://localhost:4000/auths/auth';
@@ -11,8 +11,8 @@ export default function AuthPostData(type,userData){
              .then(res => {console.log(res.data);
                 localStorage.setItem('jwt',userData.token)
                 localStorage.setItem('email',userData.email)
-                console.log('token storage',localStorage.getItem('jwt'))
-                console.log('email storeage',localStorage.getItem('email'))
+                // console.log('token storage',localStorage.getItem('jwt'))
+                // console.log('email storeage',localStorage.getItem('email'))
             }).catch(err =>{
                 console.log('login Error',err)
             })
