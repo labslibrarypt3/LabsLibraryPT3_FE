7cfe96aa6715942393ee2986e526aa135f73e97c
iradsanchez <ira.d.sanchez@gmail.com>
2019-06-15 15:46:14 -0700

    Remove extra imports from App.js

--- neighborhood-library/src/App.js
+++ neighborhood-library/src/App.js
@@ -6,17 +6,13 @@ import { StripeProvider, Elements } from "react-stripe-elements";
 
 //component imports
 import Account from "./components/Account/Account";
-import Borrowed from "./components/MyShelf/Borrowed/Borrowed";
 import Header from "./components/Header/Headers";
 import Landing from "./components/Landing/Landing";
-import Loaned from "./components/MyShelf/Loaned/Loaned";
 import MyShelf from "./components/MyShelf/MyShelf";
-// import Borrowed from "./components/MyShelf/Borrowed/Borrowed";
-import Nav from "./components/Nav/Nav";
-import Search from "./components/Search/Search";
 import SearchGoodreads from "./components/AddBook/SearchGoodreads";
 import StripePayment from "./components/Stripe/StripePayment";
 import TwilioApp from "./components/Twilio/TwilioApp";
+
 class App extends React.Component {
   constructor() {
     super();
