fa906a05953b65f534b6b8d36aee3070a3cad698
iradsanchez <ira.d.sanchez@gmail.com>
2019-06-15 13:37:56 -0700

    forgot to save before last commit, sorry

--- neighborhood-library/src/components/MyShelf/Borrowed/Borrowed.js
+++ neighborhood-library/src/components/MyShelf/Borrowed/Borrowed.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-<<<<<<< HEAD
 import { NavLink, withRouter } from "react-router-dom";
-=======
-//This MyBook, imported below on line 5, is not the same as the one in HomeLibrary. I copied it here in case it needs its own stuff.
-// If it's the same as the one in HomeLibrary, delete one.
-import MyBook from "./MyBook";
-import { NavLink, withRouter, Redirect } from "react-router-dom";
->>>>>>> 073f96300eeec053792a36b0aecef2813f5f9192
 
 class Borrowed extends Component {
   constructor(props) {
@@ -22,22 +15,11 @@ class Borrowed extends Component {
       if (localStorage.getItem("jwt")) {
         const authToken = localStorage.getItem("jwt");
         return axios
-<<<<<<< HEAD
           .get(endpoint, { headers: { authorization: authToken } })
           .then(res => {
             this.setState({ data: res.data });
           })
           .catch(err => err.json({ error: err }));
-=======
-        .get(endpoint, {headers:{authorization:localStorage.getItem("jwt")}})
-        .then(res => {
-          // console.log(...res.data);
-          this.setState({ data: res.data });
-        })
-          .catch(err => {
-            console.log(" Error", err);
-          });
->>>>>>> 073f96300eeec053792a36b0aecef2813f5f9192
       } else {
         return <Redirect to={"/"} />;
       }
