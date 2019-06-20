26246645b8c16e2fafd7b869af1cfd389a105e08
iradsanchez <ira.d.sanchez@gmail.com>
2019-06-19 19:24:13 -0700

    Add MyBook.js to use for all of MyShelf subcomponents. Have not tested yet.

---
+++ neighborhood-library/src/components/MyShelf/MyBook.js
@@ -0,0 +1,33 @@
+//if this works on Loaned.js, Borrowed.js, and HomeLibrary.js, then use this for all 3 and delete the extras
+import React from "react";
+import axios from "axios";
+
+const MyBook = props => {
+  return (
+    <div key={props.bookId} className="book">
+      <div className="book-top">
+        <img src={props.cover} className="book-cover" />
+      </div>
+      <div className="book-bottom">
+        <p className="book-title">{props.title}</p>
+        <p>by {props.authors}</p>
+        <button
+          onClick={function buttonClicked(e) {
+            const book = props.bookId;
+            return axios({
+              method: "DELETE",
+              url: "http://localhost:4000/api/books/",
+              data: {
+                bookId: props.bookId
+              }
+            });
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MyBook;
