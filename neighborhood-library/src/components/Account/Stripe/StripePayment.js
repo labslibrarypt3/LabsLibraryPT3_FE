86d992ee8e786fb25b64a9669b3be9b26a6be9ff
iradsanchez <ira.d.sanchez@gmail.com>
2019-06-21 22:24:26 -0700

    Add state setup and button to begin setting up Stripe Connect

--- neighborhood-library/src/components/Account/Stripe/StripePayment.js
+++ neighborhood-library/src/components/Account/Stripe/StripePayment.js
@@ -4,52 +4,98 @@ import {
   injectStripe,
   ReactStripeElements
 } from "react-stripe-elements";
+import axios from "axios";
 
 class StripePayment extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "",
-      amount: ""
+      setupBegan: false,
+      isLoadingFieldsNeeded: true,
+      error: null
     };
   }
 
+  componentWillMount() {
+    this.fetchFieldsNeeded();
+  }
 
-  handleSubmit = async e => {
-    e.preventDefault();
-    try {
-      let { token } = await this.props.stripe.createToken({
-        name: this.state.name
-      });
-      let amount = this.state.amount;
-      console.log(token);
-      //fix this URL
-
-      // await fetch('http://localhost:4000/api/striperoutes/charge', {
-      await fetch(
-        "https://pt3-neighborhood-library-back.herokuapp.com/api/striperoutes/charge",
-        {
-          method: "POST",
-          headers: {
-            "Content-type": "text/plain"
-          },
-          body: token.id,
-          amount
+  fetchFieldsNeeded = () => {
+    axios
+      .post("localhost:4000/api/striperoutes/account/setup", {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
         }
-      );
-    } catch (e) {
-      throw e;
-
-    }
-    console.log("clicked!");
+      })
+      .then(res => res.json())
+      .then(json => {
+        const { success, message, setupBegan } = json;
+        // ↑ same as const success = json.success
+        success
+          ? this.setState({ setupBegan, isLoadingFieldsNeeded: false })
+          : this.setState({ error: message, isLoadingFieldsNeeded: false });
+      })
+      .catch();
   };
 
-  handleInputChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+  onClickBeginSetup = () => {
+    console.log("onClickBeginSetup");
   };
+
+  // handleSubmit = async e => {
+  //   e.preventDefault();
+  //   try {
+  //     let { token } = await this.props.stripe.createToken({
+  //       name: this.state.name
+  //     });
+  //     let amount = this.state.amount;
+  //     console.log(token);
+  //     //fix this URL
+
+  //     // await fetch('http://localhost:4000/api/striperoutes/charge', {
+  //     await fetch(
+  //       "https://pt3-neighborhood-library-back.herokuapp.com/api/striperoutes/charge",
+  //       {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-type": "text/plain"
+  //         },
+  //         body: token.id,
+  //         amount
+  //       }
+  //     );
+  //   } catch (e) {
+  //     throw e;
+  //   }
+  //   console.log("clicked!");
+  // };
+
+  // handleInputChange = e => {
+  //   this.setState({
+  //     [e.target.name]: e.target.value
+  //   });
+  // };
+
   render() {
+    const { isLoadingFieldsNeeded, setupBegan, error } = this.state;
+    if (isLoadingFieldsNeeded) {
+      return <p>...loading</p>;
+    }
+
+    if (!setupBegan) {
+      return (
+        <div>
+          <div>{error ? <p>{error}</p> : null}</div>
+          <button onClick={this.onClickBeginSetup}>Begin Setup</button>
+          <p>
+            We partner with Stripe to facilitate transferring late fees and lost
+            fees from borrowers to lenders. By clicking setup you agree to
+            Stripe's terms of service.
+          </p>
+        </div>
+      );
+    }
     return (
       <div className="stripe">
         <form className="payment-form" onSubmit={this.handleSubmit}>
