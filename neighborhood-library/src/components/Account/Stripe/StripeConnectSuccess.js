b57c8269841545d309e38cca0dc1180849f26602
iradsanchez <ira.d.sanchez@gmail.com>
2019-06-27 10:59:52 -0700

    Add Stripe confirmation component

---
+++ neighborhood-library/src/components/Account/Stripe/StripeConnectSuccess.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const StripeConnectSuccess = () => {
+  return (
+    <div>
+      <h2>You're all set!</h2>
+      <p>
+        You've successfullyconnected to Stripe and can accept payments for late
+        or lost books.
+      </p>
+      <p>Now is a great time to add a book to your library!</p>
+    </div>
+  );
+};
+
+export default StripeConnectSuccess;
