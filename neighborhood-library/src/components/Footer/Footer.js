39210d1f156348e853a1e47a6758ac88f10f5339
iradsanchez <ira.d.sanchez@gmail.com>
2019-06-29 15:54:14 -0700

    Add Terms of Service and Privacy Policy

--- neighborhood-library/src/components/Footer/Footer.js
+++ neighborhood-library/src/components/Footer/Footer.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import TOS from "../Legal/TOS";
+import Privacy from "../Legal/Privacy";
 
 const Footer = () => {
   return (
     <div>
       <footer>
- <p>A Lambda Labs project.</p>
-      <a href="#">View the source code.</a>
-      <div className="legal-links">
-        <a href="#" />Terms of Service</a>
-      <a href="#" />Privacy Policy</a>
-      </div>
-    </footer>
+        <p>A Lambda Labs project.</p>
+        <a href="#">View the source code.</a>
+        <div className="legal-links">
+          <Link to="/tos" component={TOS} />
+          <Link to="/privacy" component={Privacy} />
+        </div>
+      </footer>
     </div>
   );
 };
--- neighborhood-library/src/components/Legal/TOS.js
+++ neighborhood-library/src/components/Legal/TOS.js
@@ -1,7 +1,160 @@
 import React from "react";
 
 const TOS = () => {
-  return <div />;
+  return (
+    <div>
+      <h2>Welcome to Neighborhood Library</h2>
+      <p>
+        These terms and conditions outline the rules and regulations for the use
+        of Neighborhood Library's Website.
+      </p>{" "}
+      <br />
+      <span style="text-transform: capitalize;"> Neighborhood Library</span> is
+      located at:
+      <br />
+      <address>
+        <br />
+      </address>
+      <p>
+        By accessing this website we assume you accept these terms and
+        conditions in full. Do not continue to use Neighborhood Library's
+        website if you do not accept all of the terms and conditions stated on
+        this page.
+      </p>
+      <p>
+        The following terminology applies to these Terms and Conditions, Privacy
+        Statement and Disclaimer Notice and any or all Agreements: "Client",
+        "You" and "Your" refers to you, the person accessing this website and
+        accepting the Company's terms and conditions. "The Company",
+        "Ourselves", "We", "Our" and "Us", refers to our Company. "Party",
+        "Parties", or "Us", refers to both the Client and ourselves, or either
+        the Client or ourselves. All terms refer to the offer, acceptance and
+        consideration of payment necessary to undertake the process of our
+        assistance to the Client in the most appropriate manner, whether by
+        formal meetings of a fixed duration, or any other means, for the express
+        purpose of meeting the Client's needs in respect of provision of the
+        Company's stated services/products, in accordance with and subject to,
+        prevailing law of . Any use of the above terminology or other words in
+        the singular, plural, capitalisation and/or he/she or they, are taken as
+        interchangeable and therefore as referring to same.
+      </p>
+      <h2>License</h2>
+      <p>
+        Unless otherwise stated, Neighborhood Library and/or it's licensors own
+        the intellectual property rights for all material on Neighborhood
+        Library. All intellectual property rights are reserved. You may view
+        and/or print pages from https://goofy-mayer-45bb20.netlify.com/ for your
+        own personal use subject to restrictions set in these terms and
+        conditions.
+      </p>
+      <p>You must not:</p>
+      <ol>
+        <li>Republish material from https://goofy-mayer-45bb20.netlify.com/</li>
+        <li>
+          Sell, rent or sub-license material from
+          https://goofy-mayer-45bb20.netlify.com/
+        </li>
+        <li>
+          Reproduce, duplicate or copy material from
+          https://goofy-mayer-45bb20.netlify.com/
+        </li>
+      </ol>
+      <p>
+        Redistribute content from Neighborhood Library (unless content is
+        specifically made for redistribution).
+      </p>
+      <h2>Reservation of Rights</h2>
+      <p>
+        We reserve the right at any time and in its sole discretion to request
+        that you remove all links or any particular link to our Web site. You
+        agree to immediately remove all links to our Web site upon such request.
+        We also reserve the right to amend these terms and conditions and its
+        linking policy at any time. By continuing to link to our Web site, you
+        agree to be bound to and abide by these linking terms and conditions.
+      </p>
+      <h2>Removal of links from our website</h2>
+      <p>
+        If you find any link on our Web site or any linked web site
+        objectionable for any reason, you may contact us about this. We will
+        consider requests to remove links but will have no obligation to do so
+        or to respond directly to you.
+      </p>
+      <p>
+        Whilst we endeavour to ensure that the information on this website is
+        correct, we do not warrant its completeness or accuracy; nor do we
+        commit to ensuring that the website remains available or that the
+        material on the website is kept up to date.
+      </p>
+      <h2>Content Liability</h2>
+      <p>
+        We shall have no responsibility or liability for any content appearing
+        on your Web site. You agree to indemnify and defend us against all
+        claims arising out of or based upon your Website. No link(s) may appear
+        on any page on your Web site or within any context containing content or
+        materials that may be interpreted as libelous, obscene or criminal, or
+        which infringes, otherwise violates, or advocates the infringement or
+        other violation of, any third party rights.
+      </p>
+      <h2>Disclaimer</h2>
+      <p>
+        To the maximum extent permitted by applicable law, we exclude all
+        representations, warranties and conditions relating to our website and
+        the use of this website (including, without limitation, any warranties
+        implied by law in respect of satisfactory quality, fitness for purpose
+        and/or the use of reasonable care and skill). Nothing in this disclaimer
+        will:
+      </p>
+      <ol>
+        <li>
+          limit or exclude our or your liability for death or personal injury
+          resulting from negligence;
+        </li>
+        <li>
+          limit or exclude our or your liability for fraud or fraudulent
+          misrepresentation;
+        </li>
+        <li>
+          limit any of our or your liabilities in any way that is not permitted
+          under applicable law; or
+        </li>
+        <li>
+          exclude any of our or your liabilities that may not be excluded under
+          applicable law.
+        </li>
+      </ol>
+      <p>
+        The limitations and exclusions of liability set out in this Section and
+        elsewhere in this disclaimer: (a) are subject to the preceding
+        paragraph; and (b) govern all liabilities arising under the disclaimer
+        or in relation to the subject matter of this disclaimer, including
+        liabilities arising in contract, in tort (including negligence) and for
+        breach of statutory duty.
+      </p>
+      <p>
+        To the extent that the website and the information and services on the
+        website are provided free of charge, we will not be liable for any loss
+        or damage of any nature.
+      </p>
+      <h2>
+        Paying and Processing Late Fees, Lost Book Fees, and Other Payment
+        Transfers
+      </h2>
+      <p>
+        Payment processing services for users on Neighborhood Library are
+        provided by Stripe and are subject to the Stripe Connected Account
+        Agreement, which includes the Stripe Terms of Service (collectively, the
+        “Stripe Services Agreement”). By agreeing to these terms or continuing
+        to operate as a user of Neighborhood Library, you agree to be bound by
+        the Stripe Services Agreement, as the same may be modified by Stripe
+        from time to time. As a condition of Neighborhood Library enabling
+        payment processing services through Stripe, you agree to provide
+        Neighborhood Library accurate and complete information about you and
+        your business, and you authorize Neighborhood Library to share it and
+        transaction information related to your use of the payment processing
+        services provided by Stripe.
+      </p>
+    </div>
+  );
 };
 
 export default TOS;
