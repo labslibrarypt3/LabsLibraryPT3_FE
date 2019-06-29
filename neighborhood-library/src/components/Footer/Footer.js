2a817b0fb5857f815c82bb1263641dd4b7ae2d20
iradsanchez <ira.d.sanchez@gmail.com>
2019-06-29 15:28:33 -0700

    forgot to pull from development

--- neighborhood-library/src/components/Footer/Footer.js
+++ neighborhood-library/src/components/Footer/Footer.js
@@ -1,12 +1,17 @@
-import React from "react";
-import "../../App.css";
+import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
-      <p>A Lambda Labs project.</p>
+    <div>
+      <footer>
+ <p>A Lambda Labs project.</p>
       <a href="#">View the source code.</a>
+      <div className="legal-links">
+        <a href="#" />Terms of Service</a>
+      <a href="#" />Privacy Policy</a>
+      </div>
     </footer>
+    </div>
   );
 };
 
