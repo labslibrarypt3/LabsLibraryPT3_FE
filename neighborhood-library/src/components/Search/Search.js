ea9963e8380b2dd3e1b59b793b0d9201d229ebcf
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-07-05 13:30:35 -0400

    refactored search component

--- neighborhood-library/src/components/Search/Search.js
+++ neighborhood-library/src/components/Search/Search.js
@@ -1,32 +1,59 @@
-import React, { Component } from 'react'
-import MapContainer from './MapContainer';
+import React, { Component } from "react";
+import axios from 'axios';
+
+import MapContainer from "./MapContainer";
 
 class Search extends Component {
   state = {
-    query: '',
-  }
+    query: "",
+    searchType: "books",
+    mapProps: {}
+  };
 
-  handleInputChange = () => {
+  handleInputChange = ({ target }) => {
     this.setState({
-      query: this.search.value
-    })
-  }
+      [target.name]: target.value
+    });
+  };
+
+  onSubmitHandler = () => {
+    const { query, searchType } = this.state;
+    axios
+     .get(`http://localhost:4000/api/goodreads/search?q=${query}`)
+     .then(({ data }) => {
+       const book = data.books[0];
+       // const {  } = book;
+       console.log(data.books[0]);
+     })
+     .catch(err => {
+       console.log(err);
+     });
+  };
 
   render() {
+    const {
+      query,
+      mapProps: { searchQuery, searchType }
+    } = this.state;
     return (
       <div>
-        <form>
-          <input
-            placeholder="Search for..."
-            ref={input => this.search = input}
-            onChange={this.handleInputChange}
-          />
-          <p>{this.state.query}</p>
-        </form>
-        <MapContainer />
+        <input
+          placeholder="Search for..."
+          onChange={this.handleInputChange}
+          name="query"
+        />
+        <select onChange={this.handleInputChange} name="searchType">
+          <option>books</option>
+          <option>User Libraries</option>
+        </select>
+        <button onClick={this.onSubmitHandler}>Submit</button>
+        <p>{query}</p>
+        {searchType && searchQuery && (
+          <MapContainer searchQuery={searchQuery} searchType={searchType} />
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default Search
+export default Search;
