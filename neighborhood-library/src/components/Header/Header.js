c2817b44ea897216857e999e5ba022e45bb4f6df
fbt328 <katherine.rust.andrews@gmail.com>
2019-07-18 19:40:16 -0700

    header CSS tweaking

--- neighborhood-library/src/components/Header/Header.js
+++ neighborhood-library/src/components/Header/Header.js
@@ -24,10 +24,10 @@ const Header = props => (
         <Link to="/chat">Chat</Link>
         </li>
         <li>
-          <a href="#">borrowed</a>
+        <Link to="/account">Account</Link>
         </li>
         <li>
-          <a href="/auth">Login</a>
+          <Link to="/auth">Login</Link>
         </li>
       </ul>
     </nav>
