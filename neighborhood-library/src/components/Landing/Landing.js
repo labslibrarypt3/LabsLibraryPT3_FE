6a378ef41342907b18b2b5b4fc9df97800ae65ec
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-07-23 23:19:00 -0400

    created landing component

---
+++ neighborhood-library/src/components/Landing/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = props => {
+  return (
+    <div>
+      <h2>Neighborhood Library</h2>
+      LandingPage
+    </div>
+  );
+}
+
+export default LandingPage;
