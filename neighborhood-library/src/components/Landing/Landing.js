3a79c96449c555c7e08ce065b86ab901cb20e18d
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-07-24 21:03:00 -0400

    created simple page layout

--- neighborhood-library/src/components/Landing/Landing.js
+++ neighborhood-library/src/components/Landing/Landing.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LandingPage = props => {
   return (
     <div>
       <h2>Neighborhood Library</h2>
-      LandingPage
+      <div>
+        <p>LandingPage</p>
+      </div>
+      <div>
+        <h3>Short blurb about app</h3>
+        <p>
+          Culpa o et culpa aliquip, pariatur quis voluptate. Probant elit mandaremus,
+          mandaremus dolore magna sed amet. Te proident aut arbitror, quis ab nam sunt.
+        </p>
+      </div>
+      <div>
+        <h3>Share Your Books</h3>
+        <p>
+          Culpa o et culpa aliquip, pariatur quis voluptate. Probant elit mandaremus,
+          mandaremus dolore magna sed amet. Te proident aut arbitror, quis ab nam sunt.
+        </p>
+      </div>
+      <div>
+        <h3>Map to assist search</h3>
+        <p>
+          Culpa o et culpa aliquip, pariatur quis voluptate. Probant elit mandaremus,
+          mandaremus dolore magna sed amet. Te proident aut arbitror, quis ab nam sunt.
+        </p>
+      </div>
+      <Link to="/auth">Register/Login</Link>
     </div>
   );
 }
