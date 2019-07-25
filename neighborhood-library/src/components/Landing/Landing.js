4fa6aaaae52d6b6370ea77a271c8575ac5ed0b64
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-07-24 23:07:48 -0400

    moved image assets

--- neighborhood-library/src/components/Landing/Landing.js
+++ neighborhood-library/src/components/Landing/Landing.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import books from './assets/alfons-morales-YLSwjSy7stw-unsplash.jpg';
+import libraryMarker from './assets/noun_Library_2474696.svg';
+import messages from './assets/noun_messages_699935.svg';
 
 const LandingPage = props => {
   return (
@@ -23,11 +26,24 @@ const LandingPage = props => {
         </p>
       </div>
       <div>
-        <h3>Map to assist search</h3>
-        <p>
-          Culpa o et culpa aliquip, pariatur quis voluptate. Probant elit mandaremus,
-          mandaremus dolore magna sed amet. Te proident aut arbitror, quis ab nam sunt.
-        </p>
+        <img src={libraryMarker} alt="library marker" />
+        <div>
+          <h3>Map to assist search</h3>
+          <p>
+            Culpa o et culpa aliquip, pariatur quis voluptate. Probant elit mandaremus,
+            mandaremus dolore magna sed amet. Te proident aut arbitror, quis ab nam sunt.
+          </p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h3>Secure in app messaging</h3>
+          <p>
+            Culpa o et culpa aliquip, pariatur quis voluptate. Probant elit mandaremus,
+            mandaremus dolore magna sed amet. Te proident aut arbitror, quis ab nam sunt.
+          </p>
+        </div>
+        <img src={messages} alt="chat messages" />
       </div>
       <Link to="/auth">Register/Login</Link>
     </div>
