d63f0bc4488ff6d8f748c050c058fb9e5ea6eeb8
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-07-26 00:13:22 -0400

    refactored component layout added classNames to component

--- neighborhood-library/src/components/Landing/Landing.js
+++ neighborhood-library/src/components/Landing/Landing.js
@@ -1,49 +1,59 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import books from './assets/alfons-morales-YLSwjSy7stw-unsplash.jpg';
-import libraryMarker from './assets/noun_Library_2474696.svg';
-import messages from './assets/noun_messages_699935.svg';
+import './Landing.css';
+import Marker from './assets/noun_Library_2474696.svg';
+import Messages from './assets/noun_messages_699935.svg';
 
 const LandingPage = props => {
   return (
-    <div>
-      <h2>Neighborhood Library</h2>
-      <div>
-        <p>LandingPage</p>
+    <div className='wrapper'>
+      <div className='section parallax bg1'>
+        <h1>Neighborhood Library</h1>
       </div>
-      <div>
-        <h3>Short blurb about app</h3>
+      <div className='section static'>
         <p>
           Culpa o et culpa aliquip, pariatur quis voluptate. Probant elit mandaremus,
           mandaremus dolore magna sed amet. Te proident aut arbitror, quis ab nam sunt.
         </p>
       </div>
-      <div>
-        <h3>Share Your Books</h3>
-        <p>
-          Culpa o et culpa aliquip, pariatur quis voluptate. Probant elit mandaremus,
-          mandaremus dolore magna sed amet. Te proident aut arbitror, quis ab nam sunt.
-        </p>
+      <div className='section parallax bg2'>
+        <h1>Share Your Books</h1>
       </div>
-      <div>
-        <img src={libraryMarker} alt="library marker" />
-        <div>
-          <h3>Map to assist search</h3>
+      <div className='section-2 static'>
+        <div className='section'>
           <p>
-            We've intergrated maps and geolocation services to allow you to search libraries near you. It doesn't matter if you are in your local neighborhood, or across the globe, you will be able to borrow a good book.
+            Culpa o et culpa aliquip, pariatur quis voluptate. Probant elit mandaremus,
+            mandaremus dolore magna sed amet. Te proident aut arbitror, quis ab nam sunt.
           </p>
         </div>
-      </div>
-      <div>
-        <div>
-          <h3>Secure in app messaging</h3>
-          <p>
-            Communicate securely whith other users to coordinate exchanges.
-          </p>
+        <div className='section column static'>
+          <div className='row'>
+            <div className='row-cell'>
+              <img className='icon' src={Marker} alt='book marker' />
+            </div>
+            <div className='row-cell column'>
+              <h3>Location Search</h3>
+              <p>
+                We've intergrated maps and geolocation services to allow you to search libraries near you. It doesn't matter if you are in your local neighborhood, or across the globe, you will be able to borrow a good book.
+              </p>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='row-cell column'>
+              <h3>Secure in App Messaging</h3>
+              <p>
+                We Tilio's Chat API to allow you to communicate securely with other users to coordinate exchanges.
+              </p>
+            </div>
+            <div className='row-cell'>
+              <img className='icon' src={Messages} alt='chat bubbles' />
+            </div>
+          </div>
         </div>
-        <img src={messages} alt="chat messages" />
       </div>
-      <Link to="/auth">Register/Login</Link>
+      <div className='section parallax bg3'>
+        <Link to="/auth">Register or Login</Link>
+      </div>
     </div>
   );
 }
