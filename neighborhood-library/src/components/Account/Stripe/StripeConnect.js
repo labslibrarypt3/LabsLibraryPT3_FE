411413c81fa8deef3ca3602e9a495d46fd4303c6
iradsanchez <ira.d.sanchez@gmail.com>
2019-07-28 14:59:56 -0700

    bring url from backend to frontend for testing possible cors error

--- neighborhood-library/src/components/Account/Stripe/StripeConnect.js
+++ neighborhood-library/src/components/Account/Stripe/StripeConnect.js
@@ -1,7 +1,12 @@
 import React from "react";
 const baseUrl = process.env.REACT_APP_BASE_URL;
+const stripeClientId = process.env.REACT_APP_STRIPE_CLIENT_ID;
 
 const StripeConnect = () => {
+  const stateValue = Math.random().toString(36);
+  const redirect_uri = "https://goofy-mayer-45bb20.netlify.com";
+
+  const stripeUrl = `https://connect.stripe.com/express/oauth/authorize?redirect_uri=${redirect_uri}&client_id=${stripeClientId}&state=${stateValue}&suggested_capabilities[]=platform_payments`;
   return (
     <section className="stripe-connect">
       <p>
@@ -10,7 +15,7 @@ const StripeConnect = () => {
         <br />
         Connect to Stripe by following the link below:
       </p>
-      <a href={`${baseUrl}/api/stripe/connect`}>
+      <a href={stripeUrl}>
         <img src={require("./connect-to-stripe-button.png")} />
       </a>
     </section>
