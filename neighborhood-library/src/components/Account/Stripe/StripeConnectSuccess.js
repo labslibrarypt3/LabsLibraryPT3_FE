62a025397c4a3028b2edc44e0d93d5459c14fb98
iradsanchez <ira.d.sanchez@gmail.com>
2019-07-29 21:08:40 -0700

    convert stripe connect success to class component

--- neighborhood-library/src/components/Account/Stripe/StripeConnectSuccess.js
+++ neighborhood-library/src/components/Account/Stripe/StripeConnectSuccess.js
@@ -1,19 +1,27 @@
 import React from "react";
 import axios from "axios";
-const StripeConnectSuccess = () => {
-  //get token from the url
-  // send code to stripe
-  // fetch the user acct id
-  return (
-    <div>
-      <h2>You're all set!</h2>
-      <p>
-        You've successfully connected to Stripe and can accept payments for late
-        or lost books.
-      </p>
-      <p>Now is a great time to add a book to your library!</p>
-    </div>
-  );
-};
+class StripeConnectSuccess extends React.Component {
+  constructor() {
+    super(props);
+  }
+
+  componentDidMount() {
+    //get token from the url
+    // send code to stripe
+    // fetch the user acct id
+  }
+  render() {
+    return (
+      <div>
+        <h2>You're all set!</h2>
+        <p>
+          You've successfully connected to Stripe and can accept payments for
+          late or lost books.
+        </p>
+        <p>Now is a great time to add a book to your library!</p>
+      </div>
+    );
+  }
+}
 
 export default StripeConnectSuccess;
