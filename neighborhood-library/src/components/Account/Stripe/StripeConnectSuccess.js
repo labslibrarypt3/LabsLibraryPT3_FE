914ff0645db4639ef763659b881455d0bb007f8d
Thomas Utsey <tom@edgegluer.com>
2019-07-29 21:44:38 -0400

    fix typo

--- neighborhood-library/src/components/Account/Stripe/StripeConnectSuccess.js
+++ neighborhood-library/src/components/Account/Stripe/StripeConnectSuccess.js
@@ -5,7 +5,7 @@ const StripeConnectSuccess = () => {
     <div>
       <h2>You're all set!</h2>
       <p>
-        You've successfullyconnected to Stripe and can accept payments for late
+        You've successfully connected to Stripe and can accept payments for late
         or lost books.
       </p>
       <p>Now is a great time to add a book to your library!</p>
